18c05d2be64087422dbdecd547916f47d7717a4e
noslowdwn <noslowdwn@gmail.com>
2024-11-08 20:38:02 +0100

    finalized class

--- src/main/java/groundbreaking/newbieguard/listeners/PlayerConnectionListener.java
+++ src/main/java/groundbreaking/newbieguard/listeners/PlayerConnectionListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-public class PlayerConnectionListener implements Listener {
+public final class PlayerConnectionListener implements Listener {
 
     private final NewbieGuard plugin;
     private final DatabaseHandler database;
